73105ed2faa953d101180aae4c5625d3111d0bc1
ShubhamWadka12 <shubhamwadkar880@gmail.com>
2025-12-20 07:31:29 +0000

    Updating Files

--- Python-for-DevOps/DAY-01/check_cpu.py
+++ Python-for-DevOps/DAY-01/check_cpu.py
@@ -1,10 +1,10 @@
 import psutil
 
 def check_cpu_usage():
-
+    cpu_limit = int(input("Enter CPU usage limit : "))
     cpu_usage = psutil.cpu_percent(interval=1)
 
-    if cpu_usage > 75:
+    if cpu_usage > cpu_limit:
         print("High CPU usage detected! ❌")
     else:
         print("CPU normal ✅")
