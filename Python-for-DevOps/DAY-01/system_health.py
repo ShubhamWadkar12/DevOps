84dd658ed300d94b197585c675077a9e7d509c6d
ShubhamWadka12 <shubhamwadkar880@gmail.com>
2025-12-20 07:38:21 +0000

    Updated Files

--- Python-for-DevOps/DAY-01/system_health.py
+++ Python-for-DevOps/DAY-01/system_health.py
@@ -3,7 +3,7 @@ import psutil
 def check_cpu_usage():
     cpu_limit = int(input("Enter CPU usage limit : "))
     cpu_usage = psutil.cpu_percent(interval=1)
-
+    print(f"Current CPU Usage: {cpu_usage}%")
     if cpu_usage > cpu_limit:
         print("High CPU usage detected! ❌")
     else:
